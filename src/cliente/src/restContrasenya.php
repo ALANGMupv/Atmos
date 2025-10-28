3b19852b96f1926c73748616df729fc4ddc407a2
santifuenma <sanfuenmayor@gmail.com>
2025-10-28 12:00:46 +0100

    Create restContrasenya.php

---
+++ src/cliente/src/restContrasenya.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Iniciar Sesión - Atmos</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+
+<main>
+    <section class="login-container">
+
+        <!-- Icono X (cerrar) -->
+        <img src="icons/cerrar.svg" alt="Cerrar" class="icono-cerrar">
+
+        <!-- Logo -->
+        <img src="img/logoAtmos.png" alt="Logo Atmos" class="logo-login">
+
+        <!-- Texto de introducción -->
+        <p class="texto-intro">
+            Introduce tus credenciales de inicio de sesión
+        </p>
+    </section>
+</main>
+
+</body>
+</html>
